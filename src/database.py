c284deece2e4ba7362150f64bd74e9549311e8db
estevan-ulian <estevan.ulian@gmail.com>
2026-06-07 18:28:37 -0300

    refactor: extract async session factory in database module

--- src/database.py
+++ src/database.py
@@ -1,14 +1,15 @@
 from typing import AsyncGenerator
-from sqlmodel.ext.asyncio.session import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from src.config import config
 
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from src.config import config
 
 async_engine = create_async_engine(url=config.DATABASE_URL, echo=True)
 
+AsyncSessionLocal = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    Session = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
-
-    async with Session() as session:
+    async with AsyncSessionLocal() as session:
         yield session
